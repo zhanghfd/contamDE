9e36e46b2386600dcbf08a8637223ebf02220fe1
zhanghfd <zhanghfd@fudan.edu.cn>
2016-11-03 19:54:17 +0800

    first commit

--- R/unmatched.R
+++ R/unmatched.R
@@ -2,13 +2,13 @@ unmatched <-
 function(data,In=In){ #In:Number of normal sample
 
 	W.all = NULL;
-  
+
   data = as.matrix(data);
 
   In = In;
 	Ic = ncol(data)-In;
   J = nrow(data);
-  
+
 
   Y = data[,1:In];
   Z = data[,-(1:In)];
@@ -56,8 +56,8 @@ function(data,In=In){ #In:Number of normal sample
 		### estimation of mu, delta, alpha; likelihood ratio test
 		for(m in 1:J){
 			phij = Phi.est[m];
-			yj = Y[m,];
-			zj = Z[m,];
+			yj = data[m,1:In];
+			zj = data[m,-(1:In)];
 
 			# minus loglikelihood function under alternative hypothesis
 			f1=function(mu){  #mu = c(alpha,(1...,I-1),muj,deltaj)
@@ -68,12 +68,12 @@ function(data,In=In){ #In:Number of normal sample
 
 				mu1.phi = 1/(1 + mj * phij); ##vector of length I.
 				mu2.phi = 1/(1 + (mj + W.est*deltaj) * phij);
-					
+
 				L1 = 1/phij * log(mu1.phi) + yj * log(1-mu1.phi);
 				L2 = 1/phij * log(mu2.phi) + zj * log(1-mu2.phi);
 				return(-sum(L1) - sum(L2));
 			}
-			
+
 			g1=function(mu){
 
 				###par:
@@ -83,15 +83,15 @@ function(data,In=In){ #In:Number of normal sample
 
 				mu1.phi = 1 + mj * phij; ##the inverse of the mu1.phi above in the f1 function.
 				mu2.phi = 1 + (mj+W.est* deltaj) * phij;
-				
+
 				L1 = -1/mu1.phi + phij*yj/((mu1.phi-1)*mu1.phi);
 				L2 = -1/mu2.phi + phij*zj/((mu2.phi-1)*mu2.phi);
 				partial_mj = sum(L1)+ sum(L2);
-				
+
 				partial_deltaj =  sum(-W.est/mu2.phi+phij*W.est*zj/((mu2.phi-1)*mu2.phi));
 				return (-c(partial_mj,partial_deltaj));
 			}
-		
+
 			# minus loglikelihood function under null hypothesis
 			f0=function(mu){  ##mu = muj
 				###par:
@@ -112,14 +112,14 @@ function(data,In=In){ #In:Number of normal sample
 				#######
 
 				mu.phi = 1 + mj * phij;  #the inverse of the mu.phi function in the f0 func above
-			
+
 				partial_mj = sum(-1/mu.phi+ yj/(mu.phi*mj)) + sum(-1/mu.phi+ zj/(mu.phi*mj));
-				
+
 				return(-partial_mj);
 			}
-      
+
 			##### parameter estimation under H1
-			mm = mu0[m];
+			mm = mean(data[m,1:In]);
 			dd = delta0[m];
 			if(dd < 0){
 				if(-dd*W.max > mm){
@@ -136,7 +136,7 @@ function(data,In=In){ #In:Number of normal sample
 			par02 = c(par0,0);
 			res11 = nloptr(x0=par01, eval_f=f1, eval_grad_f=g1,opts=opts); # mle under alternative
 			res12 = nloptr(x0=par02, eval_f=f1, eval_grad_f=g1,opts=opts); # mle under alternative
-				
+
       val11 = res11$objective;
 			val12 = res12$objective;
 
@@ -153,7 +153,7 @@ function(data,In=In){ #In:Number of normal sample
 			delta[m] = par1[2];
 
 			# likelihood ratio test statistic
-			lrt[m] = 2*(val0-val1);    	
+			lrt[m] = 2*(val0-val1);
 			if(lrt[m] < 0) cat(m,lrt[m],'\n');
 		}
 
@@ -165,18 +165,18 @@ function(data,In=In){ #In:Number of normal sample
 		FC = log2((mu+delta)/mu);
 		FC[is.na(FC)] = -100;
 
-		###update W		
-    
+		###update W
+
     id = which(FC < 0 & p.new<1e-3);
-		
+
 		dif = Z[id,]-rowMeans(Y[id,]);
 		W.est = apply(dif,2,mean,trim=0.05);
 		W.est = as.numeric(W.est/ mean( W.est ));
 		inv.W.min = 1/min(W.est);
-		W.max = max(W.est);	
+		W.max = max(W.est);
 		W.all = rbind(W.all,W.est);
-		
-		t = t+1;		
+
+		t = t+1;
 	}
 	LR = cbind(lrt=lrt,p.value=p.new,logFC=FC);
 	return(list(W=W.all[2,],LR=LR));
